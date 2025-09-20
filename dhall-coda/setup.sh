0b21535446f6d1e342c6aae9268d9c00faf733d5
Herdi <herdisaleh02@gmail.com>
2025-09-20 23:36:56 +0200

    Added assets

--- dhall-coda/setup.sh
+++ dhall-coda/setup.sh
@@ -5,4 +5,4 @@ wget https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-
 wget https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-json-1.7.12-x86_64-linux.tar.bz2
 tar --extract --file dhall-1.42.2-x86_64-linux.tar.bz2
 tar --extract --file dhall-json-1.7.12-x86_64-linux.tar.bz2
-rm *.bz2
+rm -r *.bz2 ./share/
