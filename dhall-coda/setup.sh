e9a6ce6facd160a7ccf38d1f820f84542f23713d
Herdi <herdisaleh02@gmail.com>
2025-09-20 23:01:36 +0200

    Setup, download interpreter and tools

--- dhall-coda/setup.sh
+++ dhall-coda/setup.sh
@@ -1,1 +1,11 @@
-wget https://github.com/Herdi2/dhall-tutorial/blob/main/main.dhall
+# Setup working environment
+
+# Tutorial files conain all Dhall files to use
+wget https://github.com/Herdi2/dhall-tutorial/tree/main/tutorial-files
+
+# All dhall tools, including interpreter and config generators
+wget https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-1.42.2-x86_64-linux.tar.bz2
+wget https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-json-1.7.12-x86_64-linux.tar.bz2
+tar --extract --file dhall-1.42.2-x86_64-linux.tar.bz2
+tar --extract --file dhall-json-1.7.12-x86_64-linux.tar.bz2
+rm *.bz2
